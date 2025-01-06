9dc32c5d81ee33451762217949dfbb0bfa2e116f
sathvik <sathvik.rao@iiitb.ac.in>
2025-01-06 21:57:22 +0530

    implemented basic rag with query expansion and chat history

--- query_search_service.py
+++ query_search_service.py
@@ -1,29 +1,106 @@
-# There is also a way to query the search service using the inbuilt service_preview command in SQL on the snowflake website
-# Check that as well
-
-from connector import *
+from connector import connect
 from snowflake.core import Root
+import json
 
-def query_search(session):
+class RAG:
+    def __init__(self):
+        self.session = connect()
+        self.root = Root(self.session)
+        self.cortex_search_service = (
+            self.root
+            .databases["MLLM"]
+            .schemas["PUBLIC"]
+            .cortex_search_services["PDF_SEARCH_SERVICE"]
+        )
+    
+    def _query_expansion(self, text, chat_history):
+        history_context = "\n".join(
+            [f"User: {msg['user']}\nAssistant: {msg['assistant']}" for msg in chat_history]
+        )
 
-    load_dotenv()
-    database = os.getenv("SNOWFLAKE_DATABASE")
+        system_prompt = f"""You are an AI Language model assistant. Your task is to generate five different versions of the given user query to retrieve relevant documents from a vector database. 
+By generating multiple perspectives on the user query and the given chat history, your goal is to overcome some of the limitations of distance-based search. 
 
-    root = Root(session)
-    my_service = (root
-    .databases[database]
-    .schemas["PUBLIC"]
-    .cortex_search_services["pdf_search_v2"]
-    )
+Chat History:
+{history_context}
 
-    resp = my_service.search(
-    query="cross and self attention",
-    columns=["chunk_id", "content"],
-    limit=5
-    )
-    print(resp.to_json())
+Provide the alternative versions separated by a newline character."""
+
+        # Prepare the SQL query
+        sql_query = """SELECT SNOWFLAKE.CORTEX.COMPLETE(
+        'mistral-large',
+        [
+            {{
+                'role': 'system', 'content': '{}'
+            }},
+            {{
+                'role': 'user', 'content': '{}'
+            }}
+        ],
+        {{ }}
+) AS response
+""".format(system_prompt, text)
+
+        res = self.session.sql(sql_query).collect()
+        res = res[0].RESPONSE
+        res = eval(res)["choices"][0]["messages"].replace("\\n", "\n").split("\n")
+        return res
+        
+    def _search(self, text):
+        resp = self.cortex_search_service.search(
+            query=text,
+            columns=["chunk_id", "content"],
+            limit=5
+        )
+        resp = eval(resp.to_json())["results"]
+        return resp
+    
+    def _get_context(self, text, chat_history):
+        queries = self._query_expansion(text, chat_history)
+        queries = [text]
+        res = set()
+        for query in queries:
+            if query.strip() != "":
+                for r in self._search(query):
+                    res.add(r["content"])
+    
+        context = ""
+        for r in res:
+            context += r+"\n\n"
+        return context
+    
+    def response(self, text, chat_history):
+        context = self._get_context(text, chat_history)
+        system_prompt = """You are an AI Language model assistant. Your task is to generate a response to the user query based on the given context. Do not repeat the query again, just generate a response based on the context provided."""
+        context="Context: "+context+"\n\n"+"Query: "+text
+        context=json.dumps(context)
+        system_prompt=json.dumps(system_prompt)
+            
+        
+        response_query = f"""SELECT SNOWFLAKE.CORTEX.COMPLETE(
+            'mistral-large',
+            [
+                {{
+                    'role': 'system', 'content': '{system_prompt[1:-1]}'
+                }},
+                {{
+                    'role': 'user', 'content': '{context[1:-1]}'
+                }}
+            ],
+            {{ 'guardrails': True }}
+        ) AS response"""
+        res = self.session.sql(response_query).collect()
+        
+        res = res[0].RESPONSE
+        res = eval(res)["choices"][0]["messages"]
+        return res
 
 
 if __name__ == "__main__":
-    session = connect()
-    query_search(session)
+    rag = RAG()
+    text = "What are masked autoencoders?"
+    chat_history = [
+    ]
+
+    docs = rag.response(text, chat_history)
+    print(docs)
